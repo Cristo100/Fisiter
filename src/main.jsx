7f609fcdc7437b47227561a62936f254c02d6a3d
Cristo100 <cristobalpichara@gmail.com>
2025-06-06 18:04:42 -0400

    Estructura Base del proyecto utilizando React

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const App = () => {
+  return <h1>Hola Fisiternpm start</h1>
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
